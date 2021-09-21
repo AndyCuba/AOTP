ec219e1e2e3f6e82c6d896cb1d2c3afc33404056
Andy Daytona <zfesandydaytona@gmail.com>
2021-09-21 15:40:48 +0300

    [AOTP] settings component finished

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants';
-import { Button, Switch } from 'react-native';
+import { Button, Switch, View } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '../../components/Navigation/Navigation';
 import FadeInView from '../../components/FadeInView/FadeInView';
-import { StyledSettingsView, StyledSettingsWrapper } from './style';
+import { StyledBackground, StyledHeading, StyledMusicSwitch, StyledSettingsView, StyledSettingsWrapper, StyledSwitch } from './style';
 import AppText from '../../components/AppText/AppText';
 import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import { musicSelector } from '../../redux/reducers/musicReducer/musicSelectors';
 import { toggleMusic } from '../../redux/reducers/musicReducer/musicReducer';
+import { StyledLink, StyledLinkText } from '../IntroPage/style';
 
 type ProfileScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -34,15 +35,26 @@ export default function SettingsPage({navigation}: Props) {
             <StatusBar style="auto" hidden={true} />
             {/* <FadeInView> */}
                 <StyledSettingsWrapper>
-                    <AppText>Settings</AppText>
-                    <AppText>Music:</AppText>
-                    <Switch
-                        trackColor={{ false: "#767577", true: "#f4f3f4" }}
-                        thumbColor={isMusicPlaying ? "#46b000" : "#f4f3f4"}
-                        ios_backgroundColor="#3e3e3e"
-                        value={isMusicPlaying}
-                        onValueChange={handleMusicChange} />
-                    <Button title="В меню" onPress={() => navigation.navigate('Intro')} />
+                    <StyledBackground 
+                        source={require('../../../assets/settings_bg.jpeg')} 
+                        resizeMode='cover'
+                        borderRadius={10}
+                    >
+                        <StyledHeading>Настройки</StyledHeading>
+                        <StyledMusicSwitch>
+                            <StyledLinkText>Музыка:</StyledLinkText>
+                            <StyledSwitch
+                                trackColor={{ false: "#767577", true: "#f4f3f4" }}
+                                thumbColor={isMusicPlaying ? "#46b000" : "#f4f3f4"}
+                                ios_backgroundColor="#3e3e3e"
+                                value={isMusicPlaying}
+                                onValueChange={handleMusicChange}
+                            />
+                        </StyledMusicSwitch>
+                        <StyledLink to="/Intro">
+                            <StyledLinkText>В меню</StyledLinkText>
+                        </StyledLink>
+                    </StyledBackground>
                 </StyledSettingsWrapper>
             {/* </FadeInView> */}
         </StyledSettingsView> 
--- src/pages/SettingsPage/style.tsx
+++ src/pages/SettingsPage/style.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import AppText from '../../components/AppText/AppText';
 
 type ViewProps = {
     paddingTop: number,
@@ -6,14 +7,49 @@ type ViewProps = {
 
 const StyledSettingsView = styled.View<ViewProps>`
     flex: 1;
-    padding-top: ${props => props.paddingTop}px;
+    align-items: center;
+    padding: ${props => props.paddingTop}px 0 ${props => props.paddingTop - 5}px;
 `;
 
 const StyledSettingsWrapper = styled.View`
+    flex: 1;
+    width: 400px;
+    border: 1px solid gray;
+    border-radius: 10px;
+`;
 
+const StyledBackground = styled.ImageBackground`
+    flex: 1;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+const StyledHeading = styled(AppText)`
+    font-size: 30px;
+    color: white;
+    padding: 20px 0;
+`;
+
+const StyledMusicSwitch = styled.View`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: flex-end;
+    padding-bottom: 40px;
+    width: 200px;
+`;
+
+const StyledSwitch = styled.Switch`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
 `;
 
 export {
     StyledSettingsView,
     StyledSettingsWrapper,
+    StyledBackground,
+    StyledHeading,
+    StyledMusicSwitch,
+    StyledSwitch
 };
